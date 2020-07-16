7787673301b2d59088f74f7a2da1c163b633f754
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-15 21:16:02 -0500

    CRUD Complete with React-Redux

--- src/components/allPost.jsx
+++ src/components/allPost.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import Post from './post'
+import EditComponent from './editComponent'
 
-function AllPost() {
+
+class AllPost extends React.Component {
+
+  render() {
+
     return (
-        <div>
-            <h1> Todos los post</h1>
-           
-        </div>
+      <div>
+        <h1 className="post_heading">Todos los post</h1>
+        {
+          this.props.posts.map((post) => (
+            <div key={post.id}>
+              {
+                post.editing ? <EditComponent post={post} />
+                  :
+                  <Post post={post} />
+              }
+            </div>
+          )
+          )
+        }
+      </div>
     )
+  }
 }
 
-export default AllPost
+const mapStateProps = (state) => {
+  return {
+    posts: state
+  }
+}
+
+export default connect(mapStateProps)(AllPost)
--- src/components/postForm.jsx
+++ src/components/postForm.jsx
@@ -5,46 +5,42 @@ import { connect } from 'react-redux'
 
 class PostForm extends React.Component {
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const title = this.getTitle.value
-        const message = this.getMessagge.value
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const title = this.getTitle.value
+    const message = this.getMessagge.value
 
-        const data =  {
-            id: new Date(),
-            title,
-            message
-        }
-        this.props.dispatch({
-            type: 'ADD_POST',
-            data
-        })
-
-        this.getTitle.value = ''
-        this.getMessagge.value = ''
-
+    const data = {
+      id: new Date(),
+      title,
+      message,
+      editing: false
     }
+    this.props.dispatch({
+      type: 'ADD_POST',
+      data
+    })
+
+    this.getTitle.value = ''
+    this.getMessagge.value = ''
+
+  }
 
 
-    render() {
-
-        return (
-            <div>
-                <h1> Crear Post</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" ref={(input) => this.getTitle = input} placeholder="ingresa el titulo del post" />
-                    <br />
-                    <textarea name="" id="" ref={(input) => this.getMessagge = input} cols="30" rows="10" placeholder=" Escribe tu  post aqui"></textarea>
-                    <br />
-                    <input type="submit"  value='post'/>
-                </form>
-            </div>
-        )
-
-    }
-
-
-
+  render() {
+    return (
+      <div className="post-container">
+        <h1 className="post-heading"> Crear Post</h1>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <input type="text" required ref={(input) => this.getTitle = input} placeholder="ingresa el titulo del post" />
+          <br />
+          <textarea name="" id="" required ref={(input) => this.getMessagge = input} cols="30" rows="10" placeholder=" Escribe tu  post aqui"></textarea>
+          <br />
+          <input type="submit" value='post' />
+        </form>
+      </div>
+    )
+  }
 }
 
-export default PostForm
+export default connect()(PostForm)
